936ff89813b330244004ad8cf03a90c0f36ac812
Srdjan Markovic <srdjan.markovic@gmail.com>
2017-04-03 17:54:43 +0200

    Check if `room` and `name` session variables are set

--- app/main/events.py
+++ app/main/events.py
@@ -7,24 +7,26 @@ from .. import socketio
 def joined(message):
     """Sent by clients when they enter a room.
     A status message is broadcast to all people in the room."""
-    room = session.get('room')
+    room = session.get('room') if 'room' in session else 'None'
+    name = session.get('name') if 'name' in session else 'None'
     join_room(room)
-    emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
+    emit('status', {'msg': name + ' has entered the room.'}, room=room)
 
 
 @socketio.on('text', namespace='/chat')
 def text(message):
     """Sent by a client when the user entered a new message.
     The message is sent to all people in the room."""
-    room = session.get('room')
-    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
+    room = session.get('room') if 'room' in session else 'None'
+    name = session.get('name') if 'name' in session else 'None'
+    emit('message', {'msg': name + ':' + message['msg']}, room=room)
 
 
 @socketio.on('left', namespace='/chat')
 def left(message):
     """Sent by clients when they leave a room.
     A status message is broadcast to all people in the room."""
-    room = session.get('room')
+    room = session.get('room') if 'room' in session else 'None'
+    name = session.get('name') if 'name' in session else 'None'
     leave_room(room)
-    emit('status', {'msg': session.get('name') + ' has left the room.'}, room=room)
-
+    emit('status', {'msg': name + ' has left the room.'}, room=room)
